1624248b36fbc8fa1b0ed719d2ff9e7b57b8b0d3
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-04-02 00:23:34 +0530

    Created Question component and add state to show data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const apiUrl = "http://localhost:8000/questions";
 
@@ -22,6 +23,9 @@ function reducer(state, action) {
 
     case "dataFailed":
       return { ...state, status: "error" };
+
+    case "start":
+      return { ...state, status: "active" };
 
     default:
       throw new Error("Action is unknown");
@@ -57,7 +61,10 @@ function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
