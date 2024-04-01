a7e5f9e4f7afaa6d29c5d85e012484279fe2a32a
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-04-01 22:45:23 +0530

    CCreated new component Main Created fake API with local data and load them into app using useEffect hook Initiate the useReducer to handle data "ready" and "error" statuses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
+import { useEffect, useReducer } from "react";
 import "./App.css";
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+
+const apiUrl = "http://localhost:8000/questions";
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Action is unknown");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const res = await fetch(apiUrl);
+        if (!res.ok) throw new Error("Data not loading");
+
+        const data = await res.json();
+        // console.log(data);
+        dispatch({ type: "dataRecieved", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed", payload: err.message });
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
